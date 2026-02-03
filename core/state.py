8fb5fd2aba060c6ea758c813f6c61a992ca52cb4
Danthuluri Sai Hemanth varma <saihemanth>
2026-02-03 13:20:33 +0530

    fix langchain import

--- core/state.py
+++ core/state.py
@@ -2,7 +2,8 @@
 from typing import TypedDict
 from typing import List
 from typing import Optional
-from langchain.schema import Document
+# from langchain.schema import Document
+from langchain_core.documents import Document
 
 class AgentState(TypedDict):
     question: str
